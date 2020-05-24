4d7db0567c3a140518f7a7a8cf573eb7606cb1fc
magiruuvelvet <magiruuvelvet@naver.com>
2020-05-24 12:26:33 +0200

    gui: set application icon

--- gui/main.cpp
+++ gui/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char **argv)
     QApplication a(argc, argv);
     a.setApplicationName("otpgen");
     a.setApplicationDisplayName("OTPGen");
+    a.setWindowIcon(QIcon(":/app-icon.svgz"));
 
     return 0;
 }
