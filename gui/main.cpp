c3617efa1e5e520b38522d3683149ab7f80836e6
magiruuvelvet <magiruuvelvet@naver.com>
2020-05-24 17:20:47 +0200

    gui: load translations on startup

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char **argv)
 {
@@ -6,6 +7,14 @@ int main(int argc, char **argv)
     a.setApplicationName("otpgen");
     a.setApplicationDisplayName("OTPGen");
     a.setWindowIcon(QIcon(":/app-icon.svgz"));
+
+    // load embedded translations for current locale using QRC language and alias magic :)
+    // falls back to embedded English strings if no translation was found
+    QTranslator translator;
+    if (translator.load(QLocale(), ":/i18n/lang.qm"))
+    {
+        a.installTranslator(&translator);
+    }
 
     return 0;
 }
