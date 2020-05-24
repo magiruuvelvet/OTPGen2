c20436871646fa3376f7b6dfee637f3b2b16bbf3
magiruuvelvet <magiruuvelvet@naver.com>
2020-05-24 16:39:38 +0200

    i18n: add some translations

---
+++ gui/i18n/de.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en_US">
+<context>
+    <name>ActionsDelegate</name>
+    <message>
+        <location filename="../otpmodel/actionsdelegate.cpp" line="20"/>
+        <source>Make token visible</source>
+        <comment>otp</comment>
+        <translation>Token sichtbar machen</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/actionsdelegate.cpp" line="27"/>
+        <source>Copy token to clipboard</source>
+        <comment>otp</comment>
+        <translation>Token in die Zwischenablage kopieren</translation>
+    </message>
+</context>
+<context>
+    <name>OTPTokenModel</name>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="79"/>
+        <source>Actions</source>
+        <comment>otp</comment>
+        <translation>Aktionen</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="80"/>
+        <source>Type</source>
+        <comment>otp</comment>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="81"/>
+        <source>Label</source>
+        <comment>otp</comment>
+        <translation>Label</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="82"/>
+        <source>Token</source>
+        <comment>otp</comment>
+        <translation>Token</translation>
+    </message>
+</context>
+</TS>
---
+++ gui/i18n/ja_JP.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>ActionsDelegate</name>
+    <message>
+        <location filename="../otpmodel/actionsdelegate.cpp" line="20"/>
+        <source>Make token visible</source>
+        <comment>otp</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/actionsdelegate.cpp" line="27"/>
+        <source>Copy token to clipboard</source>
+        <comment>otp</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OTPTokenModel</name>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="79"/>
+        <source>Actions</source>
+        <comment>otp</comment>
+        <translation>アクション</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="80"/>
+        <source>Type</source>
+        <comment>otp</comment>
+        <translation>タイプ</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="81"/>
+        <source>Label</source>
+        <comment>otp</comment>
+        <translation>名前</translation>
+    </message>
+    <message>
+        <location filename="../otpmodel/otptokenmodel.cpp" line="82"/>
+        <source>Token</source>
+        <comment>otp</comment>
+        <translation>トークン</translation>
+    </message>
+</context>
+</TS>
