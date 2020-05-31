4c68a915a6577e1e390bfca776ef5c2113a16be3
magiruuvelvet <magiruuvelvet@naver.com>
2020-05-31 12:54:41 +0200

    core: implement encryption and file writing

--- core/tokenstore.cpp
+++ core/tokenstore.cpp
@@ -29,7 +29,7 @@ static bool loadFileContents(const std::string filePath, std::string &contents, 
     if (createMode)
     {
         std::fstream file;
-        file.open(filePath, base_flags | std::ios_base::out);
+        file.open(filePath, base_flags | std::ios_base::out | std::ios_base::trunc);
         if (file.is_open())
         {
             file.close();
@@ -52,10 +52,56 @@ static bool loadFileContents(const std::string filePath, std::string &contents, 
         // read entire file
         std::vector<char> buffer(size);
         file.read(&buffer[0], size);
+        file.close();
 
         // convert to std::string
         contents = std::string(buffer.data(), buffer.size());
         return true;
+    }
+
+    return false;
+}
+
+static bool writeFile(const std::string &filePath, const std::string &contents)
+{
+    std::fstream file;
+    file.open(filePath, std::ios_base::binary | std::ios_base::out | std::ios_base::trunc);
+    if (file.is_open())
+    {
+        file.write(contents.data(), contents.size());
+        file.flush();
+        file.close();
+        return true;
+    }
+
+    return false;
+}
+
+static bool encryptData(const std::ostringstream &input, const std::string &password, std::string &output)
+{
+    output.clear();
+
+    try {
+
+        const std::string inputData = input.str();
+
+        CryptoPP::SecByteBlock key(CryptoPP::AES::MAX_KEYLENGTH + CryptoPP::AES::BLOCKSIZE);
+        CryptoPP::HKDF<CryptoPP::SHA256> hkdf;
+        hkdf.DeriveKey(key, key.size(),
+                       reinterpret_cast<const unsigned char*>(password.data()), password.size(),
+                       reinterpret_cast<const unsigned char*>(password.data()), password.size(), nullptr, 0);
+
+        CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+        CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, reinterpret_cast<const unsigned char*>(password.data()));
+
+        CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink(output));
+        stfEncryptor.Put(reinterpret_cast<const unsigned char*>(inputData.data()), inputData.size());
+        stfEncryptor.MessageEnd();
+
+        return true;
+
+    } catch (...) {
+        output.clear();
     }
 
     return false;
@@ -90,9 +136,12 @@ TokenStore::TokenStore(const std::string &filePath, const std::string &password)
     const bool exists = std::filesystem::exists(filePath);
     const bool is_reg = std::filesystem::is_regular_file(filePath);
 
+    std::string fileContents;
+
     if (exists && is_reg)
     {
         // attempt to use existing file
+        // TODO: read from encrypted file
         return;
     }
     else if (exists)
@@ -104,6 +153,10 @@ TokenStore::TokenStore(const std::string &filePath, const std::string &password)
     else
     {
         // attempt to create a new file
+        if (!loadFileContents(this->_filePath, fileContents, true))
+        {
+            this->_state = PermissionDenied;
+        }
         return;
     }
 }
@@ -123,11 +176,22 @@ TokenStore::ErrorCode TokenStore::commit()
     }
 
     // serialize the entire thing
-    std::ostringstream buffer;
-    cereal::PortableBinaryOutputArchive archive(buffer);
+    std::ostringstream serializedTokensBuf;
+    cereal::PortableBinaryOutputArchive archive(serializedTokensBuf);
     archive(this->_tokens);
 
-    // TODO: now encrypt it
+    // encrypt the serialized data
+    std::string encryptedData;
+    if (!encryptData(serializedTokensBuf, this->_password, encryptedData))
+    {
+        return EncryptionError;
+    }
 
-    return EncryptionError;
+    // write file to disk
+    if (!writeFile(this->_filePath, encryptedData))
+    {
+        return PermissionDenied;
+    }
+
+    return NoError;
 }
